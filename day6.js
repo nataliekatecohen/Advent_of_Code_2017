3b63730456403fc13e562cee2c54fad9fa25442a
Natalie Cohen <nataliekatecohen@gmail.com>
2018-01-25 19:01:45 -0500

    Update day6.js

--- day6.js
+++ day6.js
@@ -1,1 +1,28 @@
+let input = [11,	11,	13,	7,	0,	15,	5,	5,	4,	4,	1,	1,	7,	1,	15,	11];
 
+//PART 1
+const getMaxIndex = (list) => list.reduce((currMax, num, i) => (list[currMax] < num) ? i : currMax, 0);
+const getState = (list) => list.join(" ");
+
+let seenStates = new Set();
+let count = 0;
+let state = getState(input);
+while (!seenStates.has(state)){
+  seenStates.add(state);
+  let maxIndex = getMaxIndex(input);
+  let max = input[maxIndex];
+  input[maxIndex] = 0;
+  let i = (maxIndex + 1) % input.length;
+  while (max > 0){
+    ++input[i];
+    ++i;
+    i %= input.length;
+    --max;
+  }
+  state = getState(input);
+  ++count;
+}
+
+console.log(count);
+
+//PART 2
